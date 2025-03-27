fb0c16088884742a71725c05a47b6f97dbbc57f1
antonprince <antonprince95@gmail.com>
2025-03-27 18:33:57 +0530

    basic routing

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,13 @@
 import { Text, View } from "react-native";
+import {Link} from "expo-router"
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+     className="flex-1 justify-center items-center"
     >
-      <Text>Welcome ma niggas</Text>
+    <Text className="font-bold text-[#030014] text-4xl">Welcome man </Text>
+    <Link href="/movies/avengers">Go to Avengers</Link>
     </View>
   );
 }

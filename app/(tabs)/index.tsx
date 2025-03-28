7b643ef042c343a1439813b5f3b89f80aeee35ce
antonprince <antonprince95@gmail.com>
2025-03-28 16:04:22 +0530

    search bar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,44 @@
-import { Text, View } from "react-native";
+import { Text, View,Image,ScrollView } from "react-native";
 import {Link} from "expo-router"
 import "../global.css"
+import bg from "../../assets/images/bg.png"
+import logo from "../../assets/icons/logo.png"
+import Search from "../../components/Search"
+import { useRouter } from "expo-router";
 
 export default function Index() {
+  
+  const router = useRouter()
+  
   return (
     <View
-     className="flex-1 justify-center items-center"
+     className="flex-1 bg-primary"
     >
     <Text className="font-bold text-[#030014] text-xl">Welcome man </Text>
-    {/* <Link href="/movies/avengers">Go to Avengers</Link> */}
-    <Text className="">New test text</Text>
+
+      <Image
+      source={bg}
+      className="absolute w-full z-0"
+      />
+
+      <ScrollView 
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={{minHeight:'100%', paddingBottom:10}} //applies styleing to the content inside scrollview
+      className="flex-1 px-5">
+          <Image 
+            source={logo}
+            className="w-12 h-10 mt-20 mb-5 mx-auto"
+          />
+
+
+          <View>
+            <Search
+              onPress={ ()=> router.push('/search') }
+              placeholder="Search for movie"
+            />
+          </View>
+      </ScrollView>
+
     </View>
   );
 }
---
+++ components/Search.tsx
@@ -0,0 +1,37 @@
+import {Image, View, Text,TextInput  } from 'react-native'
+import React from 'react'
+import search from "../assets/icons/search.png"
+// import { TextInput } from 'react-native-gesture-handler'
+
+
+interface Props {
+    placeholder:string,
+    onPress?:()=>void
+}
+
+const Search = ({placeholder, onPress}: Props) => {
+  return (
+    <View
+    className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'
+    >   
+        <Image 
+            source={search}
+            className='size-5'
+            resizeMode='contain'
+            tintColor="#ab8bff"
+        />
+
+        <TextInput 
+            onPress={onPress}
+            placeholder={placeholder}
+            value=''
+            onChangeText={()=>{}}
+            placeholderTextColor="#ab8bff"
+            className='flex-1 ml-2 text-white'
+        />
+      <Text>Search</Text>
+    </View>
+  )
+}
+
+export default Search
